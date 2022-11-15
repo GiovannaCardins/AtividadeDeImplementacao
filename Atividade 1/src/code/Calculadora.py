0c544c2cc6d1718ad68f041ab78b8dee45c73960
GiovannaCardins <106175075+GiovannaCardins@users.noreply.github.com>
2022-11-15 15:48:47 -0300

    Update Calculadora.py

--- Atividade 1/src/code/Calculadora.py
+++ Atividade 1/src/code/Calculadora.py
@@ -1,3 +1,4 @@
+#menu que será utilizado para a escolha do usuário quanto a operação que deseja efetuar
 itens = """
 1 - Soma
 2 - Subtração
@@ -7,14 +8,27 @@ itens = """
 
 i = 's'
 while (i == 's'):
+    #Números que serão utilizados
     n1 = int(input("Digite o primeiro número: "))
     n2 = int(input("Digite o segundo número: "))
 
     print(itens)
-
     escolha = int(input("Qual operação você gostaria de efetuar? "))
 
+    if (escolha == 1):
         
-    
+        
+    elif (escolha == 2):
+        
 
+    elif (escolha == 3):
+        
+        
+    elif (escolha == 4):
+        
+        
+    else:
+        print("Inválido")
+
+    #Pergunta que, caso a resposta seja 'n', poderá quebrar o laço de repetição
     i = input("Quer continuar calculando? (s/n)")
