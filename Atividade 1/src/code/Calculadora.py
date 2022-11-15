8e6487baf605ec302927e552b2ecd5c7c9e381a8
Isabel Sousa <107064916+MariaIZZY18@users.noreply.github.com>
2022-11-15 16:59:41 -0300

    Update Calculadora.py

--- Atividade 1/src/code/Calculadora.py
+++ Atividade 1/src/code/Calculadora.py
@@ -14,21 +14,32 @@ while (i == 's'):
 
     print(itens)
     escolha = int(input("Qual operação você gostaria de efetuar? "))
+    
+        #Após o usuário digitar os números que deseja usar na equação, irá escolher qual tipo de operação deseja executar
 
     if (escolha == 1):
+        print(n1, "+", n2, "=", n1 + n2)
         
+        #Ao escolher o numero um, será efetuada a adição dos dois termos
         
     elif (escolha == 2):
+        print(n1, "-", n2, "=", n1 - n2)
         
+        #Ao escolher o numero dois, será efetuada a subtração dos dois termos
 
-    elif (escolha == 3):
+    elif (escolha == 3)
+        print(n1, "*", n2, "=", n1*n2)
+        
+        #Ao escolher o numero três, será feito o produto dos dois termos
         
         
     elif (escolha == 4):
+        print(n1, "/", n2, "=", n1/n2)
         
+        #Ao escolher o numero quatro, será efetuada a divisão dos dois termos
         
     else:
         print("Inválido")
-
-    #Pergunta que, caso a resposta seja 'n', poderá quebrar o laço de repetição
+        
+        #Caso o usuário cometa algum erro esse mensagem aparecerá. Caso a resposta seja 'n', o laço de repetição será quebrado
     i = input("Quer continuar calculando? (s/n)")
